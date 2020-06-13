8a5983ed94b212c92cfc1c7055d8ca2e558a06f4
Terence Yep <terence.yep@gmail.com>
2020-06-13 11:33:49 -0400

    Update data-analysis.R
    Made changes to include edx course starter code.

--- data-analysis.R
+++ data-analysis.R
@@ -1,18 +1,46 @@
-library("caret")
-library("tidyverse")
-library("stringr")
-setwd("C:/Users/tyep/Desktop/ml-10M100K")
-#dl <- tempfile()
-#download.file("http://files.grouplens.org/datasets/movielens/ml-10m.zip", dl)
+################################
+# Create edx set, validation set
+################################
 
-ratings_dat <- read.table(text = gsub("::", "\t", readLines("ratings.dat")))
-colnames(ratings_dat) <- c("UserID","MovieID","Rating","Timestamp")
-ratings_df <- as.data.frame(ratings_dat)
+# Note: this process could take a couple of minutes
 
-movies_dat <- str_split_fixed(readLines("movies.dat"),"::",3)
-colnames(movies_dat) <- c("MovieID","Title","Genres")
-movies_df <- as.data.frame(movies_dat)
+if(!require(tidyverse)) install.packages("tidyverse", repos = "http://cran.us.r-project.org")
+if(!require(caret)) install.packages("caret", repos = "http://cran.us.r-project.org")
+if(!require(data.table)) install.packages("data.table", repos = "http://cran.us.r-project.org")
 
-head(movies_df)
-head(ratings_df)
-  
+# MovieLens 10M dataset:
+# https://grouplens.org/datasets/movielens/10m/
+# http://files.grouplens.org/datasets/movielens/ml-10m.zip
+
+dl <- tempfile()
+download.file("http://files.grouplens.org/datasets/movielens/ml-10m.zip", dl)
+
+ratings <- fread(text = gsub("::", "\t", readLines(unzip(dl, "ml-10M100K/ratings.dat"))),
+                 col.names = c("userId", "movieId", "rating", "timestamp"))
+
+movies <- str_split_fixed(readLines(unzip(dl, "ml-10M100K/movies.dat")), "\\::", 3)
+colnames(movies) <- c("movieId", "title", "genres")
+movies <- as.data.frame(movies) %>% mutate(movieId = as.numeric(levels(movieId))[movieId],
+                                           title = as.character(title),
+                                           genres = as.character(genres))
+
+movielens <- left_join(ratings, movies, by = "movieId")
+
+# Validation set will be 10% of MovieLens data
+set.seed(1, sample.kind="Rounding")
+# if using R 3.5 or earlier, use `set.seed(1)` instead
+test_index <- createDataPartition(y = movielens$rating, times = 1, p = 0.1, list = FALSE)
+edx <- movielens[-test_index,]
+temp <- movielens[test_index,]
+
+# Make sure userId and movieId in validation set are also in edx set
+validation <- temp %>% 
+  semi_join(edx, by = "movieId") %>%
+  semi_join(edx, by = "userId")
+
+# Add rows removed from validation set back into edx set
+removed <- anti_join(temp, validation)
+edx <- rbind(edx, removed)
+
+rm(dl, ratings, movies, test_index, temp, movielens, removed)
+
